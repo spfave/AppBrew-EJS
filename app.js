8409a315061480a95406b9aa7dfe2db38991306d
Sebastian F <sebpfave2@gmail.com>
2021-02-08 21:13:25 -0500

    Use mongo database to store inital todo items

--- app.js
+++ app.js
@@ -1,45 +1,77 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date");
 
-
-// 
+//
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// Database
+mongoose.connect("mongodb://localhost:27017/todoListDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// 
-const todos = ["Get snacks", "start TV", "Binge"];
-const workToDos = [];
+const itemSchema = {
+  name: String,
+};
+const Item = mongoose.model("Item", itemSchema);
 
+const item1 = new Item({ name: "Welcome to your todo list" });
+const item2 = new Item({ name: "Use the + button to add a new todo item" });
+const item3 = new Item({ name: "<-- check this to mark a todo complete" });
+const defaultItems = [item1, item2, item3];
+
+// const items = ["Get snacks", "start TV", "Binge"];
+// const workItems = [];
+
+// Routes
 app.get("/", (req, res) => {
-    let day = date.getDate();
-    res.render("list", { listTitle: day, newToDos: todos });
+  Item.find({}, (err, foundItems) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundItems.length === 0) {
+        Item.insertMany(defaultItems, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Success saved default todo to database");
+          }
+        });
+        res.redirect("/");
+      } else {
+        const day = date.getDate();
+        res.render("list", { listTitle: day, newListItems: foundItems });
+      }
+    }
+  });
 });
 
 app.post("/", (req, res) => {
-    let newToDo = req.body.newToDo;
+  const newItem = req.body.newItem;
 
-    if (req.body.list === "Work") {
-        workToDos.push(newToDo);
-        res.redirect("/work");
-    } else {
-        todos.push(newToDo);
-        res.redirect("/");
-    }
+  if (req.body.list === "Work") {
+    workItems.push(newItem);
+    res.redirect("/work");
+  } else {
+    items.push(newItem);
+    res.redirect("/");
+  }
 });
 
 app.get("/work", (req, res) => {
-    res.render("list", { listTitle: "Work List", newToDos: workToDos });
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about");
+  res.render("about");
 });
 
 app.listen(process.env.port, () => {
-    console.log(`Server running on port: ${process.env.port}`);
+  console.log(`Server running on port: ${process.env.port}`);
 });
